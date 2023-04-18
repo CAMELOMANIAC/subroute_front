eac7294318ce08592485791bf72892c6fd0b2178
yugyusang <yugyusang12@gmail.com>
2023-04-18 15:29:51 +0900

    리덕스 환경설정

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('Hello!')
   return (
     <div className="App">
-      <header className="App-header">
-        안녕
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    </div>
+  );
+}
+function Nav() {
+  return (
+    <div className="Nav">
+      네비게이션
     </div>
   );
 }
 
-export default App;
+export {App,Nav};
